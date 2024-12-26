1bb83712bb262dcbedfe78129ceb34cfe77a0b5a
HANANBEN <hbenazize@gmail.com>
2024-12-26 16:24:52 +0100

    commit client services

---
+++ contact_laboratory-service/src/main/java/com/example/contact_laboratory_service/services/AdressClientService.java
@@ -0,0 +1,10 @@
+package com.example.contact_laboratory_service.services;
+
+
+import org.springframework.cloud.openfeign.FeignClient;
+
+@FeignClient(name="adress-service")
+public class AdressClientService {
+
+
+}
--- contact_laboratory-service/src/main/java/com/example/contact_laboratory_service/services/LaboratoryClientService.java
+++ contact_laboratory-service/src/main/java/com/example/contact_laboratory_service/services/LaboratoryClientService.java
@@ -1,4 +1,18 @@
 package com.example.contact_laboratory_service.services;
 
-public class LaboratoryClientService {
+import com.example.contact_laboratory_service.model.Laboratory;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.hateoas.PagedModel;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@FeignClient(name="laboratory-service")
+public interface  LaboratoryClientService {
+    @GetMapping("/laboratories/{id}?projection=fullLaboratory")
+    Laboratory getLaboratoryById(@PathVariable("id") Long id);
+
+    @GetMapping("/laboratories?projection=fullLaboratory")
+    PagedModel<Laboratory> getAllLaboratories();
+
+
 }
